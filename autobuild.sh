d4d69db5f0d52f6249bda7edfd336f5a03808804
gsfcv <xihua309@163.com>
2024-09-02 21:24:45 +0800

    second push

--- autobuild.sh
+++ autobuild.sh
@@ -6,5 +6,3 @@ rm -rf `pwd`/build/*
 cd `pwd`/build &&
 	cmake .. &&
 	make
-cd ..
-cp -r `pwd`/src/include `pwd`/lib
---
+++ build/src/CMakeFiles/mprpc.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dragon/mprpc/rpc/src/logger.cc" "/home/dragon/mprpc/rpc/build/src/CMakeFiles/mprpc.dir/logger.cc.o"
+  "/home/dragon/mprpc/rpc/src/mprpcapplication.cc" "/home/dragon/mprpc/rpc/build/src/CMakeFiles/mprpc.dir/mprpcapplication.cc.o"
+  "/home/dragon/mprpc/rpc/src/mprpcchannel.cc" "/home/dragon/mprpc/rpc/build/src/CMakeFiles/mprpc.dir/mprpcchannel.cc.o"
+  "/home/dragon/mprpc/rpc/src/mprpcconfig.cc" "/home/dragon/mprpc/rpc/build/src/CMakeFiles/mprpc.dir/mprpcconfig.cc.o"
+  "/home/dragon/mprpc/rpc/src/mprpccontroller.cc" "/home/dragon/mprpc/rpc/build/src/CMakeFiles/mprpc.dir/mprpccontroller.cc.o"
+  "/home/dragon/mprpc/rpc/src/rpcheader.pb.cc" "/home/dragon/mprpc/rpc/build/src/CMakeFiles/mprpc.dir/rpcheader.pb.cc.o"
+  "/home/dragon/mprpc/rpc/src/rpcprovider.cc" "/home/dragon/mprpc/rpc/build/src/CMakeFiles/mprpc.dir/rpcprovider.cc.o"
+  "/home/dragon/mprpc/rpc/src/zookeeperutil.cc" "/home/dragon/mprpc/rpc/build/src/CMakeFiles/mprpc.dir/zookeeperutil.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/include"
+  "../example"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
